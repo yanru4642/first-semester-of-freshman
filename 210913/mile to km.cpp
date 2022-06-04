08130673ddfa0af53eacec52fe10ed767dec2e93
yanru4642 <70052820+yanru4642@users.noreply.github.com>
2022-06-04 22:59:19 +0800

    Add files via upload

---
+++ 210913/mile to km.cpp
@@ -0,0 +1,28 @@
+﻿/*
+ * Convert distance in miles to kilometers.
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdlib.h> /* system definition             */
+#include<stdio.h>  /* printf, scanf definitions     */
+
+#define KMS_PER_MILE 1.609  /* conversion contant     */
+
+int main() {
+	double miles, /* input - distance in miles.       */
+		kms;      /* output - distance in kilometers  */
+
+		/* Get the distance in miles.                     */
+	printf("Enter the distance in miles> ");
+	scanf("%lf", &miles);
+
+	/* Convert the distance to kilometers.            */
+	kms = KMS_PER_MILE * miles;
+
+	/* Display the distance in kilometers.            */
+	printf("That equals %f kilometers.\n", kms);
+
+
+	system("PAUSE");
+	return 0;
+}
